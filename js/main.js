71ccbcf31899e993a76787df7d9e0ef42126de73
Erik Strom <strom2357@gmail.com>
2015-02-03 15:39:58 -0800

    Add

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
+var apiKey = 'MDE4MTYwOTc0MDE0MjMwMDU2NTcxMzE0NA001',
+	nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
+
 var app = angular.module('myApp', []);
 
-app.controller('PlayerController', ['$scope', function($scope) {
+app.controller('PlayerController', function($scope, $http) {
 	$scope.playing = false;
 	$scope.audio = document.createElement('audio');
 	$scope.audio.src = '/media/npr.mp4'; //example hardcoded .mp4
@@ -17,7 +20,17 @@ app.controller('PlayerController', ['$scope', function($scope) {
 			$scope.stop()
 		});
 	});
-}]);
 
-app.controller('RelatedController', ['$scope', function($scope) {
-}]);
+	$http({
+		mthod: 'JSONP',
+		url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+	}).success(function(data, status) {
+		// Have JSON list of NPR stories from API now...
+	}).error(function(data, status) {
+		// Some error occurred
+	});
+});
+
+
+
+
